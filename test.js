ee964488f8e0ac0e1fba49f23e2654388ed5075e
amitjamwal05 <bikujamwal@gmail.com>
2022-08-02 15:16:52 +0530

    Data Updated!

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-let id ="";
+let id ="no";
 
 const Data = () =>{
     document.querySelector(".box").innerHTML = "";  
@@ -12,7 +12,7 @@ const Data = () =>{
       document.querySelector('.box').style.fontSize="30px";
        
     }else{
-        if(id == ''){
+        if(id == 'no'){
             let arr = getCrudData();
            if(arr == null){
             let data = [name];
@@ -22,13 +22,19 @@ const Data = () =>{
            else{
             arr.push(name);
             localStorage.setItem("crud",JSON.stringify(arr));
+            document.querySelector(".box").innerHTML ="Data inserted successfully"; 
+            document.querySelector('.box').style.cssText ="color:green;font-size:30px";
            }
            document.querySelector('#name').value = "";
-           document.querySelector(".box").innerHTML ="Data inserted successfully"; 
-           document.querySelector('.box').style.cssText ="color:green;font-size:30px";
+          
         }else{
-     
-        }
+            let arr = getCrudData();
+            arr[id] = name;
+           
+            localStorage.setItem("crud",JSON.stringify(arr));
+            document.querySelector(".box").innerHTML ="Data Updated! successfully"; 
+           
+        } document.querySelector('#name').value = "";
         Select();
     }
 }
@@ -39,7 +45,9 @@ const Select = () =>{
         let html = "";
         let srno = 1;
         for(let i in arr){
-            html = html+=`<tr><td>${srno}</td><td>${arr[i]}</td><td><a href="javascript:void(0)" onclick="deleteData(${i})">Delete</a></td><td><a href="javascript:void(0)" onclick="editData()">Edit</a></td></tr>`;
+            html = html+=`<tr><td>${srno}</td><td>${arr[i]}</td>
+            <td><a href="javascript:void(0)" onclick="editData(${i})">Edit</a></td>
+            <td><a href="javascript:void(0)" onclick="deleteData(${i})">Delete</a></td></tr>`;
         srno++;
         }
         document.querySelector('#root').innerHTML = html;
@@ -47,16 +55,20 @@ const Select = () =>{
     } 
 }
 Select();
-const edit = (editDatas) =>{
+const editData = (rid) =>{
+    id = rid;
     let arr = getCrudData();
-    document.querySelector('#name').value = arr[editDatas];
+   let x = document.querySelector('#name').value = arr[rid];
+   console.log(x)
+   
 }
-const deleteData = (dataDelete) =>{
+const deleteData = (rid) =>{
     let arr = getCrudData();
-    arr.splice(dataDelete,1);
+    arr.splice(rid,1);
     localStorage.setItem("crud",JSON.stringify(arr));
     Select();
 }
+
 function getCrudData(){
     let arr = JSON.parse(localStorage.getItem('crud'));
     return arr;
